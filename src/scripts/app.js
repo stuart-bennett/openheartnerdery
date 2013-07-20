d7a5d4b09b0777ea68552781a503cc343531de27
Stuart Bennett <stuart.bennett@live.co.uk>
2013-07-20 14:55:24 +0100

    Page transitions in working state
    Needs more work, integration with history api next

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,19 +1,32 @@
-/*global $, require, Pjax, document, test, requirejs, console, alert */
+/*global $, require, Pjax, history, document, test, requirejs, console, alert */
 
 requirejs.config({
     paths: {
         jquery: 'libs/jquery.min',
-        pjax  : 'libs/pjax.min'
+        pjax  : 'libs/pjax.min',
+        rsvp  : 'libs/rsvp.min'
     },
     shim: {
         'jquery.pjax': {
             deps: ['jquery'],
             exports: 'jQuery.fn.pjax'
+        },
+        'rsvp': {
+            exports: 'RSVP'
         }
     }
 });
 
 require(['loader'], function(loader) {
     'use strict';
-    loader.init();
+    loader.init({
+        headerSelector              : "header.site-header p",
+        selector                    : ".page-content",
+        headerInTransitionClassName : "expand",
+        headerOutTransitionClassName: "squish"
+    });
+    loader.on('updated', function(args) {
+        var newLocation = args.location;
+        history.pushState({}, newLocation, newLocation);
+    });
 });
--- src/scripts/loader.js
+++ src/scripts/loader.js
@@ -1,9 +1,20 @@
 /*global console, define, $ */
-define(["jquery"], function($) {
+define(["jquery", "rsvp"], function($, RSVP) {
     'use strict';
     var mod = {
 
-        init: function() {
+        /**
+            @options:
+                selector: The part of the html body to be replaced
+        **/
+        init: function(options) {
+            this.headerInTransitionClassName = options.headerInTransitionClassName;
+            this.headerOutTransitionClassName = options.headerOutTransitionClassName;
+            this.headerContainer = $(options.headerSelector);
+            this.container = $(options.selector);
+            this.headerSelector = options.headerSelector;
+            this.selector = options.selector;
+            RSVP.EventTarget.mixin(this);
             $("a").on("click", $.proxy(mod.handleAnchor, mod));
         },
 
@@ -25,21 +36,48 @@ define(["jquery"], function($) {
         },
 
         doNetworkCall: function(url) {
-            $.get(url, $.proxy(this.updateContainer, this));
+            var oldContent = this.container.html();
+            $.get(url).done($.proxy(function(response) {
+                this.updateContainer(response);
+                this.trigger("updated", {
+                    oldContent: oldContent,
+                    location: url
+                });
+            }, this));
         },
 
         updateContainer: function(response) {
-            var bodyContent = '';
-            bodyContent = $(response).find(".wrapper").first();
-            this.replacePageContent(bodyContent.html());
+            var bodyContent = '',
+                headerContent = '',
+                responseDom;
+
+            responseDom = $("<div>").append(response);
+            headerContent = responseDom.find(this.headerSelector);
+            bodyContent = responseDom.find(this.selector);
+            this.replaceHeaderContent(headerContent);
+            this.replacePageContent(bodyContent);
+        },
+
+        replaceHeaderContent: function(contents) {
+            this.headerContainer.on('animationend', $.proxy(function() {
+                var oldContainer = this.headerContainer;
+                oldContainer.replaceWith(contents);
+                this.headerContainer = $(this.headerSelector);
+                this.headerContainer.addClass(this.headerInTransitionClassName);
+            }, this));
+            this.headerContainer.addClass(this.headerOutTransitionClassName);
         },
 
         replacePageContent: function(contents) {
-            $(".page-content").text(contents);
+            this.container.on('animationend', $.proxy(function() {
+                var oldContainer = this.container;
+                oldContainer.replaceWith(contents);
+                this.container = $(this.selector);
+                this.container.addClass("slide-in");
+            }, this));
+            this.container.addClass("slide-out");
         }
     };
 
-    return {
-        init: mod.init
-    };
+    return mod;
 });
