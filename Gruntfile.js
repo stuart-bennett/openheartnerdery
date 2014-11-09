a8bba9c3da0245c7b2c44229613a81bd5f99cdd5
stuart-bennett <stuart.bennett@live.co.uk>
2014-11-09 19:05:30 +0000

    fix grunt build

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,6 +64,6 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.registerTask('default', ['watch']);
 	grunt.registerTask('build', ['build:prod']);
-	grunt.registerTask('build:debug', ['clean', 'compass:debug', 'jekyll:debug', 'copy:debug']);
-	grunt.registerTask('build:prod', ['clean', 'compass:prod', 'jekyll:prod', 'copy:prod']);
+	grunt.registerTask('build:debug', ['clean', 'jekyll:debug', 'copy:debug']);
+	grunt.registerTask('build:prod', ['clean', 'jekyll:prod', 'copy:prod']);
 };
